c3dc4c50a743db518aad2b2179fa34ab4246121d
elenno <elenno.chen@gmail.com>
2015-01-02 06:12:20 +0800

    返回json的bug， data如果是数组，会有""包围。 修改后不会

--- lib/json.function.php
+++ lib/json.function.php
@@ -7,7 +7,7 @@ function return_msg($result_code, $result_msg=null, $data=null)
 function return_message($result_id, $data=null)
 {
 	echo (json_encode(array('result_code'=>$GLOBALS['jsonconfig']['result'][$result_id]['result_code'], 
-	'result_msg'=>$GLOBALS['jsonconfig']['result'][$result_id]['result_msg'], 'data'=>json_encode($data))));
+	'result_msg'=>$GLOBALS['jsonconfig']['result'][$result_id]['result_msg'], 'data'=>$data)));
 }
 
 function config_reader($father, $name, $suffix)
